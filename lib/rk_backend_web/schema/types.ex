1a2b5cedf0b16fd60a9aaacac39c5385f3fdfe5b
Julio Gómez Campanario <julio@JULIOs-MacBook-Air.local>
2020-03-15 19:38:10 +0100

    Sign In & CORS
    - sign_in method changed to return the currently logged in user instead of a token.
    - CORS allowed from all origings
    - Elixir's version updated
    
    closes #12 and closes #13

--- lib/rk_backend_web/schema/types.ex
+++ lib/rk_backend_web/schema/types.ex
@@ -11,10 +11,39 @@ defmodule RkBackendWeb.Schema.Types do
     field :type, :string
   end
 
+  interface :user_entity do
+    field :id, :id
+    field :email, :string
+    field :full_name, :string 
+    field :role, list_of(:role) do
+      resolve fn role, _, _ -> {:ok, role} end
+    end
+
+    resolve_type fn
+      %{token: _},  _ ->
+        :logged_in_user
+      %{}, _ ->
+        :user
+    end
+  end
+
   object :user do
     field :id, :id
     field :email, :string
     field :full_name, :string
     field :role, list_of(:role), resolve: assoc(:role)
+
+    interface :user_entity
+  end
+
+  @desc "User currectly logged in"
+  object :logged_in_user do
+    field :id, :id
+    field :email, :string
+    field :full_name, :string
+    field :role, list_of(:role), resolve: assoc(:role)
+    field :token, :string
+
+    interface :user_entity
   end
 end
